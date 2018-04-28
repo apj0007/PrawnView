37c1fdd0e3cba371ee06c1028e541da43b5fa454
apj0007 <apj0007@alu.ubu.es>
2018-04-28 23:03:38 +0200

    Create ProcesadoImagenAutomatico.py

---
+++ ProcesadoImagen/ProcesadoImagenAutomatico.py
@@ -0,0 +1,12 @@
+from prawnvieW.ProcesadoImagen.LeerMostrar import LeerMostrar
+from prawnvieW.descargar_extraer_zip import extraer_zip,descargar_zip_url
+
+class ProcesadoImagenAutomatico():
+
+  def __init__(self):
+    self.pr_leerMostrar=LeerMostrar()
+
+  def ProcesadoImagenAutomaticoo(sef,img):
+    self.pr_leerMostrar=LeerMostrar()
+    
+    v=self.pr_leerMostrar.leer_mostrar_imagen(img)
