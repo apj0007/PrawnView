a61d98414daf5d24cadc876cc5eba1ab13ddeab4
apj0007 <apj0007@alu.ubu.es>
2018-05-03 20:27:33 +0200

    Update ProcesadoImagenAutomatico.py

--- ProcesadoImagen/ProcesadoImagenAutomatico.py
+++ ProcesadoImagen/ProcesadoImagenAutomatico.py
@@ -6,7 +6,7 @@ class ProcesadoImagenAutomatico():
   def __init__(self):
     self.pr_leerMostrar=LeerMostrar()
 
-  def ProcesadoImagenAutomaticoo(sef,img):
+  def ProcesadoImagenAutomatico(sef,img):
     self.pr_leerMostrar=LeerMostrar()
     
     v=self.pr_leerMostrar.leer_mostrar_imagen(img)
