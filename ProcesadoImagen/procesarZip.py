0cbaf13890d38f5451c3cce7507d36ac8203c3bd
apj0007 <apj0007@alu.ubu.es>
2018-05-10 20:16:33 +0200

    Update procesarZip.py

---
+++ ProcesadoImagen/procesarZip.py
@@ -0,0 +1,43 @@
+from urllib import request
+import zipfile
+
+#Función que descarga un archivo de internet
+class procesarZip():
+  
+  @classmethod
+  def descargar_zip_url(url):
+    # Nombre del archivo a partir del URL
+    zipname = url[url.rfind("/") + 1:]
+    while not zipname:
+      zipname = raw_input("No se ha podido obtener el nombre del ""archivo.\nEspecifique uno: ")
+
+    print("Descargando..." ,zipname)
+
+    # Archivo local
+    z = open(zipname, "wb")
+
+    # Escribir en un nuevo fichero local los datos obtenidos via HTTP.
+    z.write(request.urlopen(url).read())
+
+    # Cerrar ambos
+    z.close()
+
+    print ("Descargado correctamente.",zipname)
+
+    print (zipname, zipfile.is_zipfile(zipname))
+    return zipname
+
+  @classmethod
+  # abre y extrae todos los ficheros en un zip  
+  def extraer_zip(zipname):
+    password = None
+    z = zipfile.ZipFile(zipname, "r")
+    try:
+        z.extractall(pwd=password)
+        for file in z.namelist():
+          print(z.getinfo(file))
+          imshow(file)
+    except:
+        print('Error')
+        pass
+    z.close()
