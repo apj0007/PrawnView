83c4c855dc20e419e2a1394d92a1d9a0c96b52c1
apj0007 <apj0007@alu.ubu.es>
2018-05-10 20:21:24 +0200

    Update _init_.py

--- ProcesadoImagen/_init_.py
+++ ProcesadoImagen/_init_.py
@@ -1,3 +1,3 @@
 from PrawnView.ProcesadoImagen.LeerMostrar import LeerMostrar,muestra_imagenes
-from PrawnView.ProcesadoImagen.descargar_extraer_zip import extraer_zip,descargar_zip_url
+from PrawnView.ProcesadoImagen.procesarZip import procesarZip
 from PrawnView.ProcesadoImagen.ProcesadoImagenAutomatico import ProcesadoImagenAutomatico
