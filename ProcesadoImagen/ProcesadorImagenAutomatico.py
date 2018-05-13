c25bd035562eafee83c4d365a320e2d68d4fff10
apj0007 <apj0007@alu.ubu.es>
2018-05-13 18:11:27 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -3,14 +3,14 @@ from ProcesadoImagen.LeeImagen import LeeImagen
 class ProcesadorImagenAutomatico():
 
   def __init__(self):
-    self.pr_leer_imagen=leer_imagen()
-    self.pr_leer_imagen=muestra_imagenes()
+    self.pr_LeeImagen=leer_imagen()
+
 
   @classmethod
   def ProcesadorAutomatico(self,img):
 
-    self.pr_leer_imagen=leer_imagen()
-    self.pr_leer_imagen=muestra_imagenes()
+    self.pr_LeeImagen=leer_imagen()
+
     
     pr_leer_imagen.leer_imagen(img)
     pr_leer_imagen.muestra_imagenes(img,img)
