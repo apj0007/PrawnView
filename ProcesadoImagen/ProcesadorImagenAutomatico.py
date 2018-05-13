0dc46798569bfd2df2451afd5fb190dd16a737b6
apj0007 <apj0007@alu.ubu.es>
2018-05-13 13:54:10 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -4,8 +4,6 @@ class ProcesadorImagenAutomatico():
 
   def __init__(self):
     self.pr_leerMostrar=LeerMostrar()
-
-  def ProcesadorAutomatico(self,img):
-    self.pr_leerMostrar=LeerMostrar()
-    
+  @classmethod
+  def ProcesadorAutomatico(self,img):    
     self.pr_leerMostrar.leer_mostrar_imagen(img)
