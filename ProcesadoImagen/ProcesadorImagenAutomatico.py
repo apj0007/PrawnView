874877568d19c60175738dc648aaff8ec3498e65
apj0007 <apj0007@alu.ubu.es>
2018-05-13 14:15:39 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -1,25 +1,15 @@
 from PrawnView.ProcesadoImagen.LeerMostrar import LeerMostrar
-'''
+
 class ProcesadorImagenAutomatico():
 
   def __init__(self):
     self.pr_leerMostrar=LeerMostrar()
+    print("init")
   @classmethod
   def ProcesadorAutomatico(self,img):
+    print("1")
     self.pr_leerMostrar=LeerMostrar()
+    print("2")
     self.pr_leerMostrar.leer_mostrar_imagen(img)
-'''
-    
-class ProcesadorImagenAutomatico():
 
-  def __init__(self):
-    self.pr_leerMostrar=LeerMostrar()
-    
-
-  def y(self,img):
-    self.ProcesadoImagen(img)
-
-
-  def ProcesadoImagen(self,img):
-    self.pr_leerMostrar=LeerMostrar()
-    self.pr_leerMostrar.leer_mostrar_imagen(img)
+ 
