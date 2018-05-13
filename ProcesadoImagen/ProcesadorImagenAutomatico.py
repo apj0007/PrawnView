0325bb7998dad1c11035329a019163083e25ce53
apj0007 <apj0007@alu.ubu.es>
2018-05-13 12:13:15 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -1,5 +1,5 @@
 from PrawnView.ProcesadoImagen.LeerMostrar import LeerMostrar
-from PrawnView.ProcesadoImagen.ProcesarZip import ProcesarZip
+#from PrawnView.ProcesadoImagen.ProcesarZip import ProcesarZip
 from PrawnView.ProcesadoImagen.TratamientoDeImagen import TratamientoDeImagen
 
 class ProcesadorImagenAutomatico():
