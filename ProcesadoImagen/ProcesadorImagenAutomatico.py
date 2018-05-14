fda60655984b763cff3a6087dba3423266acb618
apj0007 <apj0007@alu.ubu.es>
2018-05-14 22:21:43 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -17,7 +17,7 @@ class ProcesadorImagenAutomatico():
 
     
     img=self.pr_LeeImagen.leer_imagen(path)
-    gray=self.pr_TratamientoDeImagen.escala_grises1(img)
+    gray=self.pr_TratamientoDeImagen.escala_grises(img)
     binary=self.pr_TratamientoDeImagen.binarizar(gray)
     
     self.pr_LeeImagen.muestra_imagenes([img,gray],binary)
