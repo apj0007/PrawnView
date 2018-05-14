25aaa84ea3d938449c98e488fe9c9688f182dd56
apj0007 <apj0007@alu.ubu.es>
2018-05-14 22:04:30 +0200

    Update ProcesadorImagenAutomatico.py

--- ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -1,19 +1,25 @@
 from PrawnView.ProcesadoImagen.LeeImagen import LeeImagen
 from PrawnView.ProcesadoImagen.EntradaZip import EntradaZip
+from PrawnView.ProcesadoImagen.TratamientoDeImagen import TratamientoDeImagen
 
 class ProcesadorImagenAutomatico():
 
   def __init__(self):
     self.pr_LeeImagen=LeeImagen()
+    self.pr_TratamientoDeImagen=TratamientoDeImagen()
 
 
   @classmethod
   def ProcesadorAutomatico(self,path):
 
     self.pr_LeeImagen=LeeImagen()
+    self.pr_TratamientoDeImagen=TratamientoDeImagen()
 
     
     img=self.pr_LeeImagen.leer_imagen(path)
-    self.pr_LeeImagen.muestra_imagenes([img,img])
+    gray=self.pr_TratamientoDeImagen.escala_grises1(img)
+    
+    self.pr_LeeImagen.muestra_imagenes([img,gray])
+
 
  
