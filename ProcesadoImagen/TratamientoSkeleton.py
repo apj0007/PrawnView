326a150126a2902b5c941193b48bb4bb6acd478b
apj0007 <apj0007@alu.ubu.es>
2018-05-15 23:57:50 +0200

    Update TratamientoSkeleton.py

--- ProcesadoImagen/TratamientoSkeleton.py
+++ ProcesadoImagen/TratamientoSkeleton.py
@@ -58,23 +58,6 @@ class TratamientoSkeleton():
     centro_region=[]
     area_total=0
     
-    # crea una figura con dos subfiguras
-    fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(16, 16))
-    
-    # hace las etiquetas de los ejes invisibles
-    ax[0].yaxis.set_visible(False)
-    ax[0].xaxis.set_visible(False)
-    
-    ax[1].yaxis.set_visible(False)
-    ax[1].xaxis.set_visible(False)
-    
-    # en la primera subfigura está la imagen original
-    ax[0].imshow(im)
-    
-    # en la segunda subfigura está la imagen en blanco y negro con las regiones encontradas
-    ax[1].imshow(imgBN, cmap=plt.cm.gray)
-    
-    
     # label es la primera función clave, toma una imagen en blanco y negro
     # y devuelve sus componentes conexas
     label_image = label(imgBN)
@@ -88,8 +71,7 @@ class TratamientoSkeleton():
         # draw rectangle around segmented regions
         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                   fill=False, edgecolor='red', linewidth=1)
-        ax[1].add_patch(rect)
-
+  
     plt.tight_layout()
     plt.show()
 
