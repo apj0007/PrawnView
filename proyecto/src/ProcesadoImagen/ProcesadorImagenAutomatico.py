d763da02c4a05cc9897bab3a6ad35fc640906a04
apj0007 <apj0007@alu.ubu.es>
2018-05-16 22:18:50 +0200

    Update ProcesadorImagenAutomatico.py

--- proyecto/src/ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ proyecto/src/ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -9,6 +9,11 @@ class ProcesadorImagenAutomatico():
     self.pr_LeeImagen=LeeImagen()
     self.pr_TratamientoDeImagen=TratamientoDeImagen()
     self.pr_TratamientoRegiones=TratamientoRegiones()
+  
+  @classmethod
+  def ratio(self,areag,aream):
+    ratio=areag/aream
+    return ratio
 
 
   @classmethod
@@ -27,12 +32,12 @@ class ProcesadorImagenAutomatico():
     ojo=self.pr_TratamientoRegiones.detectar_ojo(img)
     centro_langostino,area_langostino=self.pr_TratamientoRegiones.detectar_region(img,invbin)
     centro_regiones,area_total_melanosis=self.pr_TratamientoRegiones.detectar_region(img,ojo)
-    
+    rat=ratio(area_total_melanosis,area_langostino)
     
     self.pr_LeeImagen.muestra_imagenes(img)
     self.pr_LeeImagen.muestra_imagenes(gray)
     self.pr_LeeImagen.muestra_imagenes(binary)
     self.pr_LeeImagen.muestra_imagenes(sk)
     
-    print("\ncentro_langostino",centro_langostino,"\narea_langostino",area_langostino)
-    print("\ncentro_regiones",centro_regiones,"\narea_total_melanosis",area_total_melanosis)
+    return area_langostino,area_total_melanosis,rat
+    
