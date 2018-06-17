d87fa3c90e384ee9ed9dd32d0d7a58fc8a237254
apj0007 <apj0007@alu.ubu.es>
2018-06-17 13:58:07 +0200

    Update ProcesadorImagenAutomatico.py

--- proyecto/src/ProcesadoImagen/ProcesadorImagenAutomatico.py
+++ proyecto/src/ProcesadoImagen/ProcesadorImagenAutomatico.py
@@ -1,10 +1,19 @@
-from PrawnView.proyecto.src.ProcesadoImagen.LeeImagen import LeeImagen
-from PrawnView.proyecto.src.ProcesadoImagen.EntradaZip import EntradaZip
-from PrawnView.proyecto.src.ProcesadoImagen.TratamientoDeImagen import TratamientoDeImagen
-from PrawnView.proyecto.src.ProcesadoImagen.TratamientoRegiones import TratamientoRegiones
+from src.ProcesadoImagen.LeeImagen import LeeImagen
+from src.ProcesadoImagen.EntradaZip import EntradaZip
+from src.ProcesadoImagen.TratamientoDeImagen import TratamientoDeImagen
+from src.ProcesadoImagen.TratamientoRegiones import TratamientoRegiones
+
+
 
 class ProcesadorImagenAutomatico():
-
+  """
+  Clase que contendra los tratamientos referentes a la imagen, recibiendo el path y la imagen binarizada.
+  En esta clase se llama a los métodos del modulo TratamientoDeImage().Tmabien llamará
+  TratamientoRegiones() para sacar los datos referentes al area el langostino y de l area de su melanosis.
+    
+  @author: Andrés Pérez Juárez
+  @version: 1.0
+  """
   def __init__(self):
     self.pr_LeeImagen=LeeImagen()
     self.pr_TratamientoDeImagen=TratamientoDeImagen()
@@ -12,6 +21,14 @@ class ProcesadorImagenAutomatico():
   
   @classmethod
   def ratio(self,areag,aream):
+    """
+    Con este metodo recogemos las areas del langostino y el area de su melanosis para despues 
+    calcular el ratio.
+        
+    @param  areag: área de la gamba
+    @param  aream: área de la melanosis
+    @return ratio: aream/areag.
+    """
     ratio=areag/aream
     return ratio
 
@@ -19,14 +36,21 @@ class ProcesadorImagenAutomatico():
   @classmethod
   def ProcesadorAutomatico(self,path,binary):
 
+    '''
+    Método que se encarga de llamar a los modulos de TratamientoDeImagen() hasta sacar el skeleton
+    y llamada a TratamientoRegiones() para conseguir las áreas necesarias para el ratio.
+
+    @param  path: ruta de acceso a la imagen que deseamos tratar
+    @param  binary: imagen binaria
+    @return  area_langostino,area_total_melanosi: Área total del langostino y área total de la melanosis
+    '''
+
     self.pr_LeeImagen=LeeImagen()
     self.pr_TratamientoDeImagen=TratamientoDeImagen()
     self.pr_TratamientoRegiones=TratamientoRegiones()
 
     
     img=self.pr_LeeImagen.leer_imagen(path)
-    #gray=self.pr_TratamientoDeImagen.escala_grises(img)
-    #binary=self.pr_TratamientoDeImagen.binarizar(gray)
     invbin=self.pr_TratamientoDeImagen.invertirbinarizar1(binary)
     sk=self.pr_TratamientoDeImagen.skeleton(binary)
     ojo=self.pr_TratamientoRegiones.detectar_ojo(img)
@@ -34,10 +58,4 @@ class ProcesadorImagenAutomatico():
     centro_regiones,area_total_melanosis=self.pr_TratamientoRegiones.detectar_region(img,ojo)
 
     
-    self.pr_LeeImagen.muestra_imagenes(img)
-    #self.pr_LeeImagen.muestra_imagenes(gray)
-    self.pr_LeeImagen.muestra_imagenes(binary)
-    self.pr_LeeImagen.muestra_imagenes(sk)
-    
     return area_langostino,area_total_melanosis
-    
